1608d218f38bc0dd9cf919b032450da5ef3054b6
qianyewoailuo <qianyewoailuo@126.com>
2020-03-27 07:10:48 +0000

    _header&_footer

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+  <img class="brand-icon" src="https://cdn.learnku.com//uploads/communities/WtC3cPLHzMbKRSZnagU9.png!/both/44x44">
+  <a href="https://learnku.com/laravel/courses" target=_blank>
+    刻意练习，每日精进
+  </a>
+
+  <div class="float-right">
+    <a href="/about" >关于</a>
+  </div>
+</footer>
